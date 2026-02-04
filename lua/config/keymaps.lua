50a2b66e1df11e288b8b16b2f15d58356a839422
Shiluco <hirap1939@gmail.com>
2026-02-04 10:55:23 +0900

    feat:claudeから抜ける

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,3 +13,10 @@ end, { desc = "Terminal (float toggle)" })
 vim.keymap.set("t", "<C-t>", function()
   Snacks.terminal.toggle(nil, { win = { position = "float" } })
 end, { desc = "Terminal (float toggle)" })
+
+-- terminal mode 内で Ctrl + hjkl を押した時に、
+-- モードを抜けて各方向のウィンドウに移動する設定
+vim.keymap.set("t", "<C-h>", [[<C-\><C-n><C-w>h]], { desc = "Move focus to the left window" })
+vim.keymap.set("t", "<C-j>", [[<C-\><C-n><C-w>j]], { desc = "Move focus to the lower window" })
+vim.keymap.set("t", "<C-k>", [[<C-\><C-n><C-w>k]], { desc = "Move focus to the upper window" })
+vim.keymap.set("t", "<C-l>", [[<C-\><C-n><C-w>l]], { desc = "Move focus to the right window" })
